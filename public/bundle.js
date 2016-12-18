a9fa3ca73252a5dcdb0622eb51d3752b09bd5ca3
Jesse Fitzjarrell <jessefitzjarrell@yahoo.com>
2016-12-17 21:30:07 -0800

    add nav search support

--- public/bundle.js
+++ public/bundle.js
@@ -24998,7 +24998,13 @@
 
 	  onSearch: function onSearch(e) {
 	    e.preventDefault();
-	    alert("Not yet wired up!");
+	    var location = this.refs.search.value;
+	    var encodedLocation = encodeURIComponent(location);
+
+	    if (location.length > 0) {
+	      this.refs.search.value = '';
+	      window.location.hash = '#/?location' + encodedLocation;
+	    }
 	  },
 	  render: function render() {
 	    return React.createElement(
@@ -25056,7 +25062,7 @@
 	            React.createElement(
 	              'li',
 	              null,
-	              React.createElement('input', { type: 'search', placeholder: 'Search Weather by city' })
+	              React.createElement('input', { type: 'search', placeholder: 'Search Weather by city', ref: 'search' })
 	            ),
 	            React.createElement(
 	              'li',
@@ -25096,7 +25102,9 @@
 
 	    this.setState({
 	      isLoading: true,
-	      errorMessage: undefined
+	      errorMessage: undefined,
+	      location: undefined,
+	      temp: undefined
 	    });
 
 	    openWeatherMap.getTemp(location).then(function (temp) {
@@ -25111,6 +25119,22 @@
 	        errorMessage: e.message
 	      });
 	    });
+	  },
+	  componentDidMount: function componentDidMount() {
+	    var location = this.props.location.query.location;
+
+	    if (location && location.length > 0) {
+	      this.handleSearch(location);
+	      window.location.hash = '#/';
+	    }
+	  },
+	  componentWillReceiveProps: function componentWillReceiveProps(newProps) {
+	    var location = newProps.location.query.location;
+
+	    if (location && location.length > 0) {
+	      this.handleSearch(location);
+	      window.location.hash = '#/';
+	    }
 	  },
 	  render: function render() {
 	    var _state = this.state,
