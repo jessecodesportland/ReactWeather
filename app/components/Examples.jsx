7466c0dfa5adad64788ba7a10aa81ee75e05f4a8
Jesse Fitzjarrell <jessefitzjarrell@yahoo.com>
2016-12-17 19:45:03 -0800

    add error modal

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -17,10 +17,10 @@ var Examples = (props) => {
       <p>Here are a few example locations to try out:</p>
       <ol>
         <li>
-          <Link to='/?location=Philadelphia'>Philadelphia, PA</Link>
+          <Link to='/?location=Portland'>Portland, OR</Link>
         </li>
         <li>
-          <Link to='/?location=Rio'>Rio, Brazil</Link>
+          <Link to='/?location=Marquette'>Marquette, MI</Link>
         </li>
       </ol>
     </div>
